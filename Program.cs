70a70c0c336ee421ba825db78f48473addfd7ec7
=ino <=innonyamuha845@gmail.com>
2019-12-18 15:40:46 +0200

    inno

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace salaryapp
+{
+    public class SalaryCalculator()
+    {
+        private int empid;
+        private string name;
+        private string postion;
+        public SalaryCalculator(int empid, string name, string position);
+        {
+            this.empid = empid;
+            this.name = name;
+            this.postion = postion;
+            {
+
+
+
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ salarycalculator.cs
@@ -0,0 +1,12 @@
+﻿namespace salaryapp
+{
+    public class salarycalculator
+    {
+
+        public static  double calculatesalary(double gs)
+        {
+            var  salary = gs - (gs * 0.3) - (gs * 0.05) - (gs * 0.03);
+            return salary;
+        }
+    }
+}
